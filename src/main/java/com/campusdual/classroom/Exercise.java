e06d66ad99ad8eadb2625c544de7fbddee88cafa
Iván Cotelo Vilanova <ivancotelo11@gmail.com>
2025-11-03 16:04:38 +0100

    Solve Exercise 25

--- src/main/java/com/campusdual/classroom/Exercise.java
+++ src/main/java/com/campusdual/classroom/Exercise.java
@@ -1,19 +1,30 @@
 package com.campusdual.classroom;
 
+import java.util.HashMap;
 import java.util.Map;
 
 public class Exercise {
 
     public static Map<String, Person> createHashMap() {
-
+        Map<String,Person> personas = new HashMap<>();
+        personas.put("person", new Person("John", "Smith"));
+        personas.put("teacher", new Teacher("María", "Montessori", "Educación"));
+        personas.put("police", new PoliceOfficer("Jake","Peralta", "B-99"));
+        personas.put("doctor", new Doctor("Gregory", "House", "Nefrología e infectología"));
+        return personas;
     }
 
     public static Person addMapValue(Map<String, Person> map, String key, Person value) {
-
+        map.put(key, value); /// aqui o que fago e que o mapa añada o tipo de persona que é e agregue os valores o map
+        return value;
     }
 
     public static void printMapValues(Map<String, Person> map) {
-
+        for (Map.Entry<String,Person> entry : map.entrySet()){ ///  Aquí recorro todo el mapa y obtengo cada entrada (clave-valor)
+            Person person = entry.getValue();  /// Creo una variable que guarda el valor de la entrada, es decir, el objeto Person
+            person.getDetails();  ///  Llamo al método getDetails() para que me muestre los detalles del objeto por pantalla
+        }
     }
+
 
 }
